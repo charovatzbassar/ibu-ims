8c91a87acc06ad87ef2ed0920af751e777cece99
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-11-30 23:58:26 +0100

    refactor code

--- index.js
+++ index.js
@@ -4,11 +4,22 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const internshipRoutes = require("./routers/internshipRouter");
+const APIError = require("./utils/APIError");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/internships", internshipRoutes);
+
+app.all("*", (req, res, next) => {
+  next(new APIError("Page not found", 404));
+});
+
+app.use((err, req, res, next) => {
+  const { status = 500 } = err;
+  if (!err.message) err.message = "Something went wrong :/";
+  res.status(status).json({ message: err.message, status });
+});
 
 app.listen(process.env.API_PORT, () => {
   console.log("Server is listening");
--- routers/internshipRouter.js
+++ routers/internshipRouter.js
@@ -1,58 +1,70 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const { catchAsync } = require("../utils/catchAsync");
 
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get("/", async (req, res) => {
-  const data = await prisma.internship_listing.findMany();
-  res.json(data);
-});
+router
+  .route("/")
+  .get(
+    catchAsync(async (req, res) => {
+      const data = await prisma.internship_listing.findMany();
+      res.json(data);
+    })
+  )
+  .post(
+    catchAsync(async (req, res) => {
+      const newListing = await prisma.internship_listing.create({
+        data: {
+          ...req.body,
+          startDate: new Date(req.body.startDate),
+          endDate: new Date(req.body.endDate),
+        },
+      });
+      res.json(newListing);
+    })
+  );
 
-router.post("/new", async (req, res) => {
-  const newListing = await prisma.internship_listing.create({
-    data: {
-      ...req.body,
-      startDate: new Date(req.body.startDate),
-      endDate: new Date(req.body.endDate),
-    },
-  });
-  res.json(newListing);
-});
+router
+  .route("/:id")
+  .get(
+    catchAsync(async (req, res) => {
+      const listing = await prisma.internship_listing.findUnique({
+        where: {
+          listingID: Number(req.params.id),
+        },
+      });
 
-router.get("/:id", async (req, res) => {
-  const listing = await prisma.internship_listing.findUnique({
-    where: {
-      listingID: Number(req.params.id),
-    },
-  });
+      res.json(listing);
+    })
+  )
+  .patch(
+    catchAsync(async (req, res) => {
+      const updatedListing = await prisma.internship_listing.update({
+        where: {
+          listingID: Number(req.params.id),
+        },
+        data: {
+          ...req.body,
+          startDate: new Date(req.body.startDate),
+          endDate: new Date(req.body.endDate),
+        },
+      });
 
-  res.json(listing);
-});
+      res.json(updatedListing);
+    })
+  )
+  .delete(
+    catchAsync(async (req, res) => {
+      const deletedListing = await prisma.internship_listing.delete({
+        where: {
+          listingID: Number(req.params.id),
+        },
+      });
 
-router.patch("/:id/edit", async (req, res) => {
-  const updatedListing = await prisma.internship_listing.update({
-    where: {
-      listingID: Number(req.params.id),
-    },
-    data: {
-      ...req.body,
-      startDate: new Date(req.body.startDate),
-      endDate: new Date(req.body.endDate),
-    },
-  });
-
-  res.json(updatedListing);
-});
-
-router.delete("/:id/delete", async (req, res) => {
-  const deletedListing = await prisma.internship_listing.delete({
-    where: {
-      listingID: Number(req.params.id),
-    },
-  });
-
-  res.json(deletedListing);
-});
+      res.json(deletedListing);
+    })
+  );
 
 module.exports = router;
