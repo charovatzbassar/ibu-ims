a95b7d11720788bd995073e051068d55a3dcc791
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2023-12-01 15:49:36 +0100

    add company crud

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const internshipRoutes = require("./routers/internshipRouter");
+const companyRoutes = require("./routers/companyRouter");
 const APIError = require("./utils/APIError");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/internships", internshipRoutes);
+app.use("/api/companies", companyRoutes);
 
 app.all("*", (req, res, next) => {
   next(new APIError("Page not found", 404));
---
+++ routers/companyRouter.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const { catchAsync } = require("../utils/catchAsync");
+
+const router = express.Router();
+const prisma = new PrismaClient();
+
+router
+  .route("/")
+  .get(
+    catchAsync(async (req, res) => {
+      const allCompanies = await prisma.company.findMany();
+      res.json(allCompanies);
+    })
+  )
+  .post(
+    catchAsync(async (req, res) => {
+      const newCompany = await prisma.company.create({
+        data: {
+          ...req.body,
+        },
+      });
+      res.json(newCompany);
+    })
+  );
+
+router
+  .route("/:id")
+  .get(
+    catchAsync(async (req, res) => {
+      const company = await prisma.company.findUnique({
+        where: {
+          companyID: Number(req.params.id),
+        },
+      });
+
+      res.json(company);
+    })
+  )
+  .patch(
+    catchAsync(async (req, res) => {
+      const updatedCompany = await prisma.company.update({
+        where: {
+          companyID: Number(req.params.id),
+        },
+        data: {
+          ...req.body,
+        },
+      });
+
+      res.json(updatedCompany);
+    })
+  )
+  .delete(
+    catchAsync(async (req, res) => {
+      const deletedCompany = await prisma.company.delete({
+        where: {
+          companyID: Number(req.params.id),
+        },
+      });
+
+      res.json(deletedCompany);
+    })
+  );
+
+module.exports = router;
