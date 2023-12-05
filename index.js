078be2f794fdc58ae91f6d12a3761c23559b81c1
Basar Carovac <carovac.basar@gmail.com>
2023-12-05 09:42:01 +0100

    init

--- index.js
+++ index.js
@@ -9,11 +9,46 @@ const companyRoutes = require("./routers/companyRouter");
 const APIError = require("./utils/APIError");
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocs = require("./swagger/swagger-output.json");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+const { Strategy: GoogleStrategy } = require("passport-google-oauth20");
 
 const port = 8080;
 const appPort = 5173;
 
 const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.AUTH_CLIENT_ID,
+      clientSecret: process.env.AUTH_CLIENT_SECRET,
+      callbackURL: `http://localhost:${port}/auth/google/callback`,
+    },
+    (accessToken, refreshToken, profile, done) => {
+      const user = { id: profile.id, email: profile.emails[0].value };
+      return done(null, user);
+    }
+  )
+);
+
+app.use(passport.initialize());
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const token = jwt.sign({ user: req.user }, "your-secret-key", {
+      expiresIn: "1h",
+    });
+    res.redirect(`http://localhost:3000/login?token=${token}`);
+  }
+);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
