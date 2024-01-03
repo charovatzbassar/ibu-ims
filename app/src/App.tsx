7152d5c91bb25e189fbb1869fe23b59e89713e0b
Bassar Charovatz <81241353+charovatzbassar@users.noreply.github.com>
2024-01-03 14:47:57 +0100

    remove dependency

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,5 @@
-import axios from "axios";
-import { login, logout } from "./auth/auth";
-
-const getData = async () => {
-  const res = await axios.get("http://localhost:8080/api/companies", {
-    withCredentials: true,
-  });
-  console.log(res.data);
-};
-
 function App() {
-  return (
-    <>
-      <button onClick={login}>Log In</button>
-      <button onClick={logout}>Log Out</button>
-      <button onClick={getData}>Get Data</button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
