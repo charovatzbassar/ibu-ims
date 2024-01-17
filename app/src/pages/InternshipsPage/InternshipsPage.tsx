2bca7a5cfa8fe2bc97247f890d526b710e4f6b0c
Basar Carovac <carovac.basar@gmail.com>
2024-01-17 13:47:37 +0100

    loading spinner

--- app/src/pages/InternshipsPage/InternshipsPage.tsx
+++ app/src/pages/InternshipsPage/InternshipsPage.tsx
@@ -1,7 +1,23 @@
+import { useInternshipListings } from "@/hooks";
 import React from "react";
+import { CircularProgress } from "@mui/material";
 
 const InternshipsPage: React.FC = () => {
-  return <>Internships</>;
+  const { data, isPending, isError, error } = useInternshipListings();
+
+  let content;
+
+  if (isPending) {
+    content = <CircularProgress />;
+  }
+
+  if (data) {
+    content = <div>{data.map((internshipListing) => {
+      return <div key={internshipListing.listingID}>{internshipListing.position}</div>;
+    })}</div>;
+  }
+
+  return <>{content}</>;
 };
 
 export default InternshipsPage;
